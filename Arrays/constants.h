dd1dd07cbc7541748ea38c1feb873bfb92494484
Юля <@DESKTOP-LDN2BMM>
2025-04-05 13:28:17 +0300

    Separation in progress

---
+++ Arrays/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//Заголовочные файлы никогда не выносятся
+#define delimiter "\n------------------------------------\n"
+
+const int ROWS = 4;
+const int COLS = 10;
+
--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,20 +1,11 @@
-﻿#include<iostream>
-using namespace std;
+﻿#include"stdafx.h"
+#include"constants.h"
+#include"FillRand.h"
+#include"Print.h"
 
-#define delimiter "\n------------------------------------\n"
 
-const int ROWS = 4;
-const int COLS = 10;
 
-void FillRand(int arr[], const int n); //заполнение массива случайными числами
-void FillRand(double arr[], const int n);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand = 0, int maxRand = 100);
-void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
 
-void Print(int arr[], const int n);    //выводит массив на экран
-void Print(double arr[], const int n);
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
-void Print(double arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void Sort(int arr[], const int n);     //сортировка массива
 void Sort(double arr[], const int n);
@@ -111,82 +102,7 @@ void main()
 
 
 }
-void FillRand(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;            //функция rand()возвращает случайое число в диапазоне от0 до 32767
-	}
-}
-void FillRand(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 10000;
-		arr[i] /= 100;
-	}
-}
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % (maxRand - minRand) + minRand;
-		}
-	}
-}
-void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand();
-		}
-	}
-}
 
-void Print(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Print(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-void Print(double arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
 
 void Sort(int arr[], const int n)
 {
